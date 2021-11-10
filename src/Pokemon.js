662dbb7acab243e1a447323bbe401cd59cc98fce
Manish Sajwan <manish.sajwan@protonmail.com>
2021-11-10 17:56:11 +0530

    change pokemon list to cards

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,13 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Pokemon = ({name, url}) => {
-  return (
-    <Link to={`details/${name}`}>
-      <div className="pokemon-card">
-        <h3>{name}</h3>
+
+  const [pokemon, setPokemon] = useState({});
+  const [status, setStatus] = useState("loading");
+
+  useEffect (() => {
+    requestPokemonData();
+  }, [])
+
+  async function requestPokemonData () {
+
+    const res = await fetch (
+      `https://pokeapi.co/api/v2/pokemon/${name}`
+    );
+    const json = await res.json();
+
+    setPokemon(json);
+    setStatus("loaded");
+  }
+  
+  if (status === 'loaded'){
+
+    const { sprites: { other : { dream_world } } , abilities, types } = pokemon;
+    
+    console.log(dream_world);
+
+    return (
+      <div className="pokemon-card-wrapper">
+        <Link className="pokemon-card" to={`details/${name}`}>
+          <div className="card-image">
+            <img src={dream_world.front_default} />  
+          </div>
+          <h3>{name}</h3>
+        </Link>
       </div>
-    </Link>
-  )
+    )
+  } else {
+    return '';
+  }
 }
+
+
 
 export default Pokemon;
--- src/Results.js
+++ src/Results.js
@@ -4,17 +4,19 @@ import Pokemon from "./Pokemon.js";
 const Results = ({ pokemons, status }) => {
   return (
     <div className="search-results">
-      {!pokemons.length ? (
-        <h2>Fetching Data...</h2>
-      ) : (
-        pokemons.map((pokemon) => (
-          <Pokemon 
-            name={pokemon.name}
-            url={pokemon.url}
-            key={pokemon.name}
-          />
-        ))
-      )}
+      <div className="pokemon-cards">
+        {!pokemons.length ? (
+          <h2>Fetching Data...</h2>
+        ) : (
+          pokemons.map((pokemon) => (
+            <Pokemon 
+              name={pokemon.name}
+              url={pokemon.url}
+              key={pokemon.name}
+            />
+          ))
+        )}
+      </div>
     </div>
   )
 }
